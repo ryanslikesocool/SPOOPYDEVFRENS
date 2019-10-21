5a2c7aa38502998e714be813a6f02b3dfced46f8
Ryan Boyer <ryanjboyer@gmail.com>
2019-10-21 14:48:30 -0600

    Reorganized and added the first ghost!

---
+++ Assets/_Scripts/GhostData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ifelse/Ghost Data", fileName = "New Ghost Data")]
+public class GhostData : ScriptableObject
+{
+    public string discord;
+    public string twitter;
+    public AnimationClip animationClip;
+}

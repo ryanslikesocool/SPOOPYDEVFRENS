791df8415cd286aa2fb1c4e941fec4286434aec6
Ryan Boyer <ryanjboyer@gmail.com>
2019-10-24 02:01:05 -0600

    Added more ghosts

---
+++ Assets/Ghost Submissions/OceanDeep/Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformerController : PhysicsObject {
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+   
+    private Animator animator;
+    private bool facingRight;
+    // Use this for initialization
+    void Awake () 
+    {
+          
+        animator = GetComponent<Animator> ();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis ("Horizontal");
+
+        if(move.x >0 && !facingRight || move.x < 0 && facingRight)
+        Flip();
+
+        if (Input.GetButtonDown ("Jump") && grounded) {
+            velocity.y = jumpTakeOffSpeed;
+        } else if (Input.GetButtonUp ("Jump")) 
+        {
+            if (velocity.y > 0) {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+        void Flip ()
+        {
+            facingRight= !facingRight; 
+            Vector3 transformScale = transform.localScale;
+            transformScale.x *=-1;
+            transform.localScale = transformScale;
+        } 
+       
+
+        animator.SetBool ("grounded", grounded);
+        animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
+
+        targetVelocity = move * maxSpeed;
+    }
+}

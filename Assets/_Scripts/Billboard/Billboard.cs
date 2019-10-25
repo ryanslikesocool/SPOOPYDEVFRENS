3b020ed2929a886c249eb9017543dd1cdbc8649f
juul1a <juliasgreig@gmail.com>
2019-10-24 18:30:22 -0700

    billboard script

---
+++ Assets/_Scripts/Billboard/Billboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(player.transform.position);
+    }
+}
